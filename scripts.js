2798eb4e994f12a55e5f3a9abd76d1c56b69ddf0
MiguelBranidno <162053310+MiguelBranidno@users.noreply.github.com>
2024-03-03 16:13:11 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+        // Adicionando um evento de clique ao botão usando JavaScript
+        document.getElementById("botaoEnviar").addEventListener("click", function () {
+            // Redirecionando para a outra página ao clicar
+            window.location.href = "nomepessoa/nomepessoa.html";
+        });
